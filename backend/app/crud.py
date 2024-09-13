624ef9ccfa8da34cf3bda7dd8469082561e5c08d
flofriday <flohacksfriday@gmail.com>
2024-09-13 23:49:51 +0200

    Fisish the crawler

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,33 +1,48 @@
+from http.client import HTTPException
 from sqlalchemy.orm import Session
+import sqlalchemy
 from . import models, schemas, auth
+
 
 def get_user(db: Session, user_id: int):
     return db.query(models.User).filter(models.User.id == user_id).first()
 
+
 def get_user_by_username(db: Session, username: str):
     return db.query(models.User).filter(models.User.username == username).first()
 
+
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = auth.get_password_hash(user.password)
-    db_user = models.User(username=user.username, hashed_password=hashed_password, is_admin=user.is_admin)
+    db_user = models.User(
+        username=user.username, hashed_password=hashed_password, is_admin=user.is_admin
+    )
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
 
+
 def create_admin_user(db: Session, username: str, password: str):
     admin_user = schemas.UserCreate(username=username, password=password, is_admin=True)
     return create_user(db, admin_user)
 
+
 def create_image(db: Session, image: schemas.ImageCreate, image_id: int):
-    db_image = models.Image(id=image_id, **image.dict())
-    db.add(db_image)
-    db.commit()
-    db.refresh(db_image)
+    try:
+        db_image = models.Image(id=image_id, **image.dict())
+        db.add(db_image)
+        db.commit()
+        db.refresh(db_image)
+    except sqlalchemy.exc.IntegrityError:
+        raise HTTPException(status_code=409, detail="Item already exists")
+
     return db_image
+
 
 def get_image(db: Session, image_id: int):
     return db.query(models.Image).filter(models.Image.id == image_id).first()
+
 
 def create_audio(db: Session, audio: schemas.AudioCreate, user_id: int):
     db_audio = models.Audio(**audio.dict(), user_id=user_id)
@@ -36,11 +51,20 @@ def create_audio(db: Session, audio: schemas.AudioCreate, user_id: int):
     db.refresh(db_audio)
     return db_audio
 
+
 def get_audio(db: Session, audio_id: int):
     return db.query(models.Audio).filter(models.Audio.id == audio_id).first()
 
+
 def get_audios_for_image(db: Session, image_id: int, skip: int = 0, limit: int = 10):
-    return db.query(models.Audio).filter(models.Audio.image_id == image_id).offset(skip).limit(limit).all()
+    return (
+        db.query(models.Audio)
+        .filter(models.Audio.image_id == image_id)
+        .offset(skip)
+        .limit(limit)
+        .all()
+    )
+
 
 def get_audios_for_user(db: Session, user_id: int):
     return db.query(models.Audio).filter(models.Audio.user_id == user_id).all()
--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 import os
@@ -12,6 +13,7 @@ logging.basicConfig(
     level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
+token = ""
 base_url = "https://sammlung.belvedere.at"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -38,8 +40,10 @@ def crawl_belvedere_collection():
         soup.find("span", class_="max-pages").text[2:].strip().replace(".", "")
     )
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(crawl_belvedere_collection_page, range(1, page_limit + 1))
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+    #    executor.map(crawl_belvedere_collection_page, range(1, page_limit + 1))
+    for page in range(1, page_limit + 1):
+        crawl_belvedere_collection_page(page)
 
 
 def crawl_belvedere_collection_page(page: int):
@@ -82,6 +86,22 @@ def crawl_belvedere_collection_page(page: int):
 
 
 def crawl_artwork_page(url, headers):
+    id = url.split("/")[4]
+    file_name = f"belvedere_data/{id}.json"
+
+    if os.path.exists(file_name):
+        logging.info(f"Metadata in cache: {file_name}")
+        with open(file_name) as f:
+            data = json.load(f)
+        store_artwork(
+            data["title"],
+            data["artist"],
+            data["description"],
+            data["description_page"],
+            data["url"],
+        )
+        return
+
     try:
         response = session.get(url, headers=headers)
         response.raise_for_status()
@@ -107,12 +127,43 @@ def crawl_artwork_page(url, headers):
         base_url + soup.find("div", class_="detail-item-img").find("img").attrs["src"]
     )
 
+    with open(file_name, "w") as f:
+        cache_data = {
+            "url": image_url,
+            "title": title,
+            "description": description,
+            "description_page": url,
+            "artist": artist,
+        }
+        json.dump(cache_data, f)
+
     store_artwork(title, artist, description, url, image_url)
 
 
 def store_artwork(name, artist, description, url, image_url):
-    # The image
-    file_name = f"belvedere_images/{url.split("/")[4]}.jpeg"
+    # Post to the db
+    id = url.split("/")[4]
+    data = {
+        "url": image_url,
+        "title": name,
+        "description": description,
+        "description_page": url,
+        "artist": artist,
+    }
+
+    api_headers = {
+        "accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}",  # Include the token in the Authorization header
+    }
+
+    api_response = session.post(
+        f"http://localhost:8000/images/{id}", headers=api_headers, json=data
+    )
+    api_response.raise_for_status()
+
+    # Store the image in a folder
+    file_name = f"belvedere_images/{id}.jpeg"
     if os.path.exists(file_name):
         logging.info(f"Stored artwork (file cached): {file_name}")
         return
@@ -130,5 +181,31 @@ def store_artwork(name, artist, description, url, image_url):
     logging.info(f"Stored artwork: {file_name}")
 
 
+def login_backend():
+    global token
+
+    # Step 1: Get the Auth Token
+    token_url = "http://127.0.0.1:8000/token"  # Replace with actual token endpoint
+    token_data = {
+        "username": "admin",  # Replace with your username
+        "password": "secret!password",  # Replace with your password
+    }
+    token_headers = {"accept": "application/json", "Content-Type": "application/json"}
+
+    # Request token from the token endpoint
+    token_response = requests.post(token_url, headers=token_headers, json=token_data)
+
+    # Check if the request was successful
+    if token_response.status_code == 200:
+        # Extract token from the response (assuming token is in the 'access_token' field)
+        token = token_response.json().get("access_token")
+        print(f"Token obtained: {token}")
+    else:
+        print(f"Failed to obtain token. Status code: {token_response.status_code}")
+        print(token_response.text)
+        exit()
+
+
 if __name__ == "__main__":
+    login_backend()
     crawl_belvedere_collection()
