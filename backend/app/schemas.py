9411a1731923f81c7da5ce611555ead06190d44c
paulpaul168 <github@paulhoeller.at>
2024-09-13 19:23:38 +0200

    Add created_at to audio path

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class UserCreate(BaseModel):
     username: str
@@ -30,6 +31,7 @@ class AudioCreate(BaseModel):
 class Audio(AudioCreate):
     id: int
     user_id: int
+    created_at: datetime
 
     class Config:
         from_attributes = True
