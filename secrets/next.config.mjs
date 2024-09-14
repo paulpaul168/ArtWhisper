e1ef38d1ab32940e2efc78c89787a5df44246612
flofriday <flohacksfriday@gmail.com>
2024-09-14 11:40:49 +0200

    Fix authentication redirect

--- secrets/next.config.mjs
+++ secrets/next.config.mjs
@@ -1,25 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/camera',
-                permanent: false,
-            },
-            {
-                source: '/auth',
-                destination: '/camera',
-                permanent: false,
-                missing: [
-                    {
-                        type: 'cookie',
-                        key: 'authToken',
-                    },
-                ],
-            },
-        ];
-    },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/camera",
+        permanent: false,
+      },
+      {
+        source: "/auth",
+        destination: "/camera",
+        permanent: false,
+        has: [
+          {
+            type: "cookie",
+            key: "authToken",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
